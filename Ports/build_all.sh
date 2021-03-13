676fc803c1b02b18e3ce8d98c76c9c0fc366edcd
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-03-13 14:13:44 +0100

    Ports: Support building ports in randomized order
    This might help uncover missing dependencies, especially when we eventually
    call build_all.sh from some kind of cronjob CI thing.

--- Ports/build_all.sh
+++ Ports/build_all.sh
@@ -1,24 +1,39 @@
 #!/usr/bin/env bash
 
+set -e
+
+# We cannot handle port with spaces/newlines/tabs in the directory name,
+# hence SC2012 is a false-positive.
+
 clean=false
+all_files=$(ls)
+# TODO: Support '$0 clean randomize'
 case "$1" in
     clean)
         clean=true
         ;;
+    randomize)
+        # shellcheck disable=SC2012
+        all_files=$(ls | shuf)
+        ;;
+    "")
+        ;;
     *)
+        echo "USAGE: $0 [clean | randomize]"
+        exit 1
         ;;
 esac
 
 some_failed=false
 
-for file in *; do
-    if [ -d $file ]; then
-        pushd $file > /dev/null
-            dirname=$(basename $file)
+for file in $all_files; do
+    if [ -d "$file" ]; then
+        pushd "$file" > /dev/null
+            dirname=$(basename "$file")
             if [ "$clean" == true ]; then
                 ./package.sh clean_all > /dev/null 2>&1
             fi
-            if $(./package.sh > /dev/null 2>&1 ); then
+            if ./package.sh > /dev/null 2>&1 ; then
                 echo "Built ${dirname}."
             else
                 echo "ERROR: Build of ${dirname} was not successful!"

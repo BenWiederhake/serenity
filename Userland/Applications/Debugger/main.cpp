af93ca27280472fc3e0961d6b3cce09f7f7cd302
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-09-12 15:38:39 +0200

    Userland: Remove unused imports in Applications/Debugger/main.cpp

--- Userland/Applications/Debugger/main.cpp
+++ Userland/Applications/Debugger/main.cpp
@@ -7,8 +7,6 @@
 #include <AK/Assertions.h>
 #include <AK/ByteBuffer.h>
 #include <AK/OwnPtr.h>
-#include <AK/Platform.h>
-#include <AK/StringBuilder.h>
 #include <LibC/sys/arch/i386/regs.h>
 #include <LibCore/ArgsParser.h>
 #include <LibDebug/DebugInfo.h>

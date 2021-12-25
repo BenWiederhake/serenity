a31429a2afc98e8a7846a77461b3d58986891c74
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2021-12-25 19:05:05 +0100

    Ports: Support nicer output in CI while building all

--- Ports/build_all.sh
+++ Ports/build_all.sh
@@ -4,10 +4,14 @@ set -e
 
 clean=false
 verbose=false
+ci_output=false
 all_files=$(ls)
 
 for arg in "$@" ; do
     case "$arg" in
+        ci-output)
+            ci_output=true
+            ;;
         clean)
             clean=true
             ;;
@@ -18,17 +22,26 @@ for arg in "$@" ; do
             all_files=$(ls | shuf)
             ;;
         *)
-            echo "USAGE: $0 {clean | randomize | verbose}*"
+            echo "USAGE: $0 {ci-output | clean | randomize | verbose}*"
             exit 1
             ;;
     esac
 done
 
-some_failed=false
+failed_ports=()
 built_ports=""
+
+if [ "$ci_output" = true ]; then
+    echo "::group::Build order"
+    echo "$all_files"
+    echo "::endgroup::"
+fi
 
 for file in $all_files; do
     if [ -d $file ]; then
+        if [ "$ci_output" = true ]; then
+            echo "::group::Build package $file"
+        fi
         pushd $file > /dev/null
             port=$(basename $file)
             port_built=0
@@ -41,11 +54,17 @@ for file in $all_files; do
             if [ $port_built -eq 1 ]; then
                 echo "Built $port."
                 popd > /dev/null
+                if [ "$ci_output" = true ]; then
+                    echo "::endgroup::"
+                fi
                 continue
             fi
             if ! [ -f package.sh ]; then
                 echo "ERROR: Skipping $port because its package.sh script is missing."
                 popd > /dev/null
+                if [ "$ci_output" = true ]; then
+                    echo "::endgroup::"
+                fi
                 continue
             fi
 
@@ -61,8 +80,11 @@ for file in $all_files; do
                     echo "Built ${port}."
                 else
                     echo "ERROR: Build of ${port} was not successful!"
-                    some_failed=true
+                    failed_ports+=("${port}")
                     popd > /dev/null
+                    if [ "$ci_output" = true ]; then
+                        echo "::endgroup::"
+                    fi
                     continue
                 fi
             else
@@ -70,19 +92,27 @@ for file in $all_files; do
                     echo "Built ${port}."
                 else
                     echo "ERROR: Build of ${port} was not successful!"
-                    some_failed=true
+                    failed_ports+=("${port}")
                     popd > /dev/null
+                if [ "$ci_output" = true ]; then
+                    echo "::endgroup::"
+                fi
                     continue
                 fi
             fi
 
             built_ports="$built_ports $port $(./package.sh showproperty depends) "
         popd > /dev/null
+        if [ "$ci_output" = true ]; then
+            echo "::endgroup::"
+        fi
     fi
 done
 
-if [ "$some_failed" == false ]; then
-    exit 0
-else
+if (( ${#failed_ports[@]} )); then
+    echo "Failed ports:"  "${failed_ports[@]}"
     exit 1
+else
+    echo "Success!"
+    exit 0
 fi
